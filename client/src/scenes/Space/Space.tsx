83502baa1c42fb2c5a81173ea53f3ca74fe1a36b
amirmammadov <amirmammado@gmail.com>
2023-11-02 09:53:13 +0400

    add file button was added

--- client/src/scenes/Space/Space.tsx
+++ client/src/scenes/Space/Space.tsx
@@ -5,7 +5,10 @@ import FileItem from "../../components/FileItem/FileItem";
 const Space = () => {
   return (
     <div className="space">
-      <div className="space__header">Folder Name</div>
+      <div className="space__header">
+        <div className="space__header__text">Folder Name</div>
+        <button className="space__header__btn">Add File</button>
+      </div>
       <div className="space__content">
         <div className="space__content__filter">
           <img
