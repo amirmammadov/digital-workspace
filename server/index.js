1633ec4260a6d5cba4095b31b2f9d5af6e6098f8
amirmammadov <amirmammado@gmail.com>
2023-11-05 21:20:04 +0400

    cors options were changed

--- server/index.js
+++ server/index.js
@@ -20,7 +20,13 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://digital-workspace-client-amirmammadov.vercel.app/",
+  methods: ["POST", "GET"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(helmet());
 
